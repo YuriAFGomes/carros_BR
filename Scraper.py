15f8f0db890d7d3993be3338060b7bda82cdf264
yurid <yuridealbuquerque@gmail.com>
2022-06-23 22:48:52 -0300

    Saving in folders according to category

--- Scraper.py
+++ Scraper.py
@@ -19,19 +19,22 @@ def salvar_fotos(categoria,pagina,n_exemplos=10):
     soup = bs(pagina,"html.parser")
     imgs = soup.find_all('img',class_='rg_i')
 
-    for i in range(len(imgs)):
+    if categoria not in os.listdir(caminho_destino):
+        os.mkdir(f"{caminho_destino}/{categoria}")
+
+    for i in range(min(n_exemplos,len(imgs))):
         img = imgs[i]
-        filename = f"{categoria}_{i+1}.jpg"
+        filename = f"{i+1}.jpg"
         try:
-            urlretrieve(img['src'],f"{caminho_destino}/{filename}")
+            urlretrieve(img['src'],f"{caminho_destino}/{categoria}/{filename}")
         except Exception as e:
             print(e)
 
 def obter_exemplos(categorias,caminho_destino):
     for nome in categorias:
-        nome = nome.replace(" ","+")
         nome = nome.replace('\n','')
-        url = URL_BASE.replace("{}",nome)
+        nome_url = nome.replace(" ","+")
+        url = URL_BASE.replace("{}",nome_url)
         driver.get(url)
         salvar_fotos(nome,driver.page_source)
 
