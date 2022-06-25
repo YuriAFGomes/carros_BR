eb35dd76328869516e4d683b180ef6253ffdfbc2
yurid <yuridealbuquerque@gmail.com>
2022-06-25 00:18:53 -0300

    4 tests passing

--- Scraper.py
+++ Scraper.py
@@ -5,7 +5,6 @@ from bs4 import BeautifulSoup as bs
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 from config import SCRAPER_CONFIG
@@ -24,9 +23,7 @@ def obter_imgs(url,n_exemplos,driver):
         imgs = listar_imgs(driver.page_source)
     return imgs
 
-def salvar_fotos(url_base,caminho_destino,categoria,n_exemplos):
-    driver = webdriver.Firefox()
-    wait = WebDriverWait(driver,10)
+def salvar_fotos(url_base,caminho_destino,categoria,n_exemplos,driver):
     categoria = categoria.replace('\n','')
     nome_url = categoria.replace(" ","+")
     url = url_base.replace("{}",nome_url)
@@ -36,7 +33,7 @@ def salvar_fotos(url_base,caminho_destino,categoria,n_exemplos):
         os.mkdir(f"{caminho_destino}/{categoria}")
 
     img_n=1
-    for i in range(len(imgs)):
+    for i in range(n_exemplos):
         img = imgs[i]
         filename = f"{img_n}.jpg"
         try:
@@ -48,11 +45,12 @@ def salvar_fotos(url_base,caminho_destino,categoria,n_exemplos):
                 img_n+=1
             except:
                 traceback.print_exc()
-    driver.close()
 
 def obter_exemplos(url_base,categorias,caminho_destino,n_exemplos):
+    driver = webdriver.Firefox()
     for nome in categorias:
-        salvar_fotos(url_base,caminho_destino,nome,n_exemplos)
+        salvar_fotos(url_base,caminho_destino,nome,n_exemplos,driver)
+    driver.close()
 
 def main():
     with open('categorias.txt') as file:
--- tests.py
+++ tests.py
@@ -1,17 +1,17 @@
-import os
+import os, shutil
 from unittest import TestCase
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 
 
-from Scraper import listar_imgs,obter_imgs
+from Scraper import listar_imgs,obter_imgs,salvar_fotos,obter_exemplos
 from config import SCRAPER_TEST_CONFIG
 
 class TestScraper(TestCase):
     @classmethod
     def setUpClass(cls):
         cls.driver = webdriver.Firefox()
-        categorias = ['Chevrolet Opala 1977','Ford Corcel 1977']
+        cls.categorias = ['Chevrolet Opala 1977','Ford Corcel 1977']
         if 'imagens_teste' not in os.listdir():
             os.mkdir('imagens_teste')
         cls.url = SCRAPER_TEST_CONFIG['url_base'].replace("{}","Chevrolet+Opala+1977")
@@ -26,7 +26,26 @@ class TestScraper(TestCase):
         imgs = obter_imgs(self.url,150,self.driver)
         self.assertTrue(len(imgs)>=150)
 
+    def test_salvar_fotos(self):
+        salvar_fotos(self.url,'imagens_teste','Chevrolet Opala 1977',10,self.driver)
+        self.assertTrue('Chevrolet Opala 1977' in os.listdir('imagens_teste'))
+        self.assertEqual(10,len(os.listdir('imagens_teste/Chevrolet Opala 1977')))
+
+    def test_obter_exemplos(self):
+        obter_exemplos(
+            SCRAPER_TEST_CONFIG['url_base'],
+            self.categorias,
+            SCRAPER_TEST_CONFIG['caminho_destino'],
+            SCRAPER_TEST_CONFIG['n_exemplos']
+        )
+        self.assertTrue('imagens_teste' in os.listdir())
+        self.assertEqual(2,len(os.listdir('imagens_teste')))
+        self.assertTrue('Chevrolet Opala 1977' in os.listdir('imagens_teste'))
+        self.assertTrue('Ford Corcel 1977' in os.listdir('imagens_teste'))
+        self.assertEqual(10,len(os.listdir('imagens_teste/Chevrolet Opala 1977')))
+        self.assertEqual(10,len(os.listdir('imagens_teste/Ford Corcel 1977')))
+
     @classmethod
     def tearDownClass(cls):
-        os.rmdir('imagens_teste')
+        shutil.rmtree('imagens_teste')
         cls.driver.close()
